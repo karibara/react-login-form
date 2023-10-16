b578181a952690af7f3107241560df7ce990106e
Malgorzata Bozykowska <malgorzata.bozykowska@gmail.com>
2023-10-16 20:47:49 +0200

    Add useEffect to keep user logged in

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
@@ -7,15 +7,33 @@ import MainHeader from './components/MainHeader/MainHeader';
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  // the first argument is  a function, the second an array of dependencies
+  // function is run after the first component start (after return part) and only if dependencies are changed
+  useEffect(()=> {
+    const storedUserLoginInfo = localStorage.getItem("isLoggedIn")
+  
+    if (storedUserLoginInfo === "1") {
+      // updated state make component fnc run again
+      setIsLoggedIn(true)
+    }
+
+  }, [])
+  
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
+    
+    // make a key value pair in local storage in browser
+    localStorage.setItem("isLoggedIn", "1")
     setIsLoggedIn(true);
   };
-
+  
   const logoutHandler = () => {
+    localStorage.removeItem("isLoggedIn")
     setIsLoggedIn(false);
   };
+
+  
 
   return (
     <React.Fragment>
